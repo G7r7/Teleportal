3ee337d378d3d0cfd89743431dc4a0142b81a928
Germain Rullier <germain.rullier@u-psud.fr>
2023-02-16 15:37:36 +0100

    Refactor

--- Assets/Scripts/Menus/Menu.cs
+++ Assets/Scripts/Menus/Menu.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class ResetScene : MonoBehaviour
+public class Menu : MonoBehaviour
 {
-    public static void RestartGame()
+    public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
     }
 
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
 }
--- Assets/Scripts/Puzzles/CountPuzzle.cs
+++ Assets/Scripts/Puzzles/CountPuzzle.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Puzzle : MonoBehaviour
+public class CountPuzzle : MonoBehaviour
 {
     public GameObject obj;
     public Material validMaterial;
