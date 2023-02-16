5b6033990feb6083ea6afe5b6db698a21d45489b
Toufik Has <toufik.has@u-psud.fr>
2023-02-16 17:33:33 +0100

    Portal manager refactor

--- Assets/Scripts/Portals/PortalManager.cs
+++ Assets/Scripts/Portals/PortalManager.cs
@@ -1,28 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class PortalManager : MonoBehaviour
 {
     public int currentPortal;
-    private GameObject portal1;
-    private GameObject portal2;
+    public GameObject portal1;
+    public GameObject portal2;
+    public XRController controller;
 
     void Start()
     {
-        portal1 = null;
-        portal2 = null;
         currentPortal = 0;
-    }
-
-    public bool isFirstPortalSet()
-    {
-        return portal1 != null;
-    }
-
-    public bool isSecondPortalSet()
-    {
-        return portal2 != null;
     }
 
     public void setCurrentFirstPortal()
@@ -35,29 +25,35 @@ public class PortalManager : MonoBehaviour
         currentPortal = 1;
     }
 
-    public void setFirstPortal(GameObject portal)
+    public void PlacePortal()
     {
-        portal1 = portal;
-    }
+        Physics.Raycast(controller.transform.position, controller.transform.forward, out RaycastHit hit);
 
-    public void setSecondPortal(GameObject portal)
-    {
-        portal2 = portal;
-    }
-
-    public void placePortal(Vector3 position, Quaternion rotation)
-    {
         if (currentPortal == 0)
         {
-            portal1.transform.position = position;
-            portal1.transform.rotation = rotation;
+            portal1.SetActive(true);
+            portal1.transform.position = hit.point;
+            portal1.transform.rotation = hit.transform.gameObject.transform.rotation;
         }
         else
         {
-            portal2.transform.position = position;
-            portal2.transform.rotation = rotation;
+            portal2.SetActive(true);
+            portal2.transform.position = hit.point;
+            portal2.transform.rotation = hit.transform.gameObject.transform.rotation;
         }
 
+    }
+
+    public void TeleportObject(GameObject obj, int sourceId)
+    {
+        Vector3 newPosition;
+        if (sourceId == 0)
+        {
+            newPosition = portal2.transform.position;
+        } else {
+            newPosition = portal1.transform.position;
+        }
+        obj.transform.position = newPosition + new Vector3(2,2,2);
     }
 
 }
