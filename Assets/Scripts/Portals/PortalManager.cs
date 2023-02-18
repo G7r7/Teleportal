ae1c2748f536b0a66f740d16e979899b0a196a45
Germain Rullier <grullier@free.fr>
2023-02-18 18:18:43 +0100

    Teleport

--- Assets/Scripts/Portals/PortalManager.cs
+++ Assets/Scripts/Portals/PortalManager.cs
@@ -23,12 +23,16 @@ public class PortalManager : MonoBehaviour
     public void TeleportObject(GameObject obj, int sourceId)
     {
         Vector3 newPosition;
-        if (sourceId == 0) { 
-            newPosition = portal2.transform.position;
+        Quaternion rotation;
+        if (sourceId == 0) {
+            newPosition = portal2.transform.position + portal2.transform.forward * 5;
+            rotation = portal2.transform.rotation;
         } else {
-            newPosition = portal1.transform.position;
+            newPosition = portal1.transform.position + portal1.transform.forward * 5;
+            rotation = portal1.transform.rotation;
         }
         obj.transform.position = newPosition;
+        obj.transform.rotation *= rotation;
     }
 
 }
